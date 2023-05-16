627288cd49f8af3c19ef913e67dbff29eb45b41e
temirlan <zaykenovtemirlan2003@gmail.com>
2023-05-16 18:43:05 +0600

    branch_changes

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -43,7 +43,7 @@ class _HomePageState extends State<HomePage> {
                   SizedBox(
                     height: 30,
                   ),
-                  Text('Trust your translations to us',
+                  Text('Trust your translations to us this is branch',
                       style: TextStyle(
                         fontSize: 40,
                         fontWeight: FontWeight.w500,
